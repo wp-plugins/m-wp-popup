f2717ff0979a4cd8a3330f0744232ca0d0437e79
Muneeb <Muneeb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-19 00:33:36 +0000

    Version 0.3
    git-svn-id: https://plugins.svn.wordpress.org/m-wp-popup/tags/0.3@715093 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-popup.php
@@ -0,0 +1,17 @@
+<?php
+/**
+Plugin Name: WP Popup Plugin
+Plugin URI: http://muneeb.me/wp-popup-plugin/
+Description: The best WordPress Popup plugin. Period.
+Version: 0.3
+Author: Muneeb
+Author URI: http://muneeb.me/wp-popup-plugin/
+License: GPLv2 or later
+Copyright: 2012 Muneeb ur Rehman http://muneeb.me
+**/
+
+require plugin_dir_path( __FILE__ ) . 'config.php';
+
+require POPUP_PLUGIN_INCLUDE_DIRECTORY . 'functions.php';
+
+load_wpp();
