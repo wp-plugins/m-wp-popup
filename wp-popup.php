dd1870d9651e2649fec0065306e743bb80593405
Muneeb <Muneeb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-22 20:39:39 +0000

    links updated
    git-svn-id: https://plugins.svn.wordpress.org/m-wp-popup/tags/0.5@1012351 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-popup.php
+++ wp-popup.php
@@ -1,15 +1,15 @@
 <?php
 /**
 Plugin Name: WP Popup Plugin
-Plugin URI: http://rocketplugins.com/wordpress-popup-plugin/?ref=plugin_uri
+Plugin URI: http://rocketplugins.com/wordpress-popup-plugin/?ref=plugin_url
 Description: The best WordPress Popup plugin. Period.
-Version: 0.5.4
+Version: 0.5.5
 Author: Muneeb
-Author URI: http://rocketplugins.com/wordpress-popup-plugin/?ref=author_uri
+Author URI: http://rocketplugins.com/wordpress-popup-plugin/?ref=author_url
 License: GPLv2 or later
 Copyright: 2013 Muneeb ur Rehman http://muneeb.me
 
-http://rocketplugins.com/contact
+http://rocketplugins.com/contact/?ref=plugin_readme
 **/
 
 require plugin_dir_path( __FILE__ ) . 'config.php';
