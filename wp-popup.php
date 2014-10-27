c74010fb9abdf310f5041f74529c66f592861723
Muneeb <Muneeb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-27 22:00:33 +0000

    link updated
    git-svn-id: https://plugins.svn.wordpress.org/m-wp-popup/tags/0.5@1014913 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-popup.php
+++ wp-popup.php
@@ -9,7 +9,7 @@ Author URI: http://rocketplugins.com/wordpress-popup-plugin/?ref=author_url
 License: GPLv2 or later
 Copyright: 2013 Muneeb ur Rehman http://muneeb.me
 
-http://rocketplugins.com/contact/?ref=plugin_readme
+http://rocketplugins.com/contact/?ref=plugin_mainfile
 **/
 
 require plugin_dir_path( __FILE__ ) . 'config.php';
